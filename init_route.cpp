d69bd5763c508693bb353e7b3b75b2ae92b94384
haidohong <dohaimail@gmail.com>
2023-01-17 00:05:51 +0700

    Adding hospital

--- init_route.cpp
+++ init_route.cpp
@@ -12,18 +12,24 @@ public:
     Technician();
     void add_point(int point);
     void print_route();
+    void end_route();
 private:
     vector<int> route;
 };
 Technician::Technician() {
+    (this -> route).push_back(0);
 }
 void Technician::add_point(int point){
     (this -> route).push_back(point);
 }
+void Technician::end_route(){
+    (this -> route).push_back(0);
+}
 void Technician::print_route(){
+    int cnt_point = 0;
     for (int point : this -> route){
+        if (cnt_point++ > 0) cout << " -> ";
         cout << point;
-        if (point != this -> route.back()) cout << " -> ";
     }
     cout << endl;
 }
@@ -31,11 +37,14 @@ void assign_route(vector<int>& route, vector<Technician>& technicians, int num_o
     int current_technician = 0;
     for (int point : route) {
         if (point > num_of_points) {
+            technicians[current_technician].end_route();
             ++current_technician;
             continue;
         }
         technicians[current_technician].add_point(point);
     }
+    technicians[current_technician].end_route();
+
 }
 int main(){
     srand(time(NULL));

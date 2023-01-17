1872d7ada94a92540e04ddecbf7d9d5dd5f9d82c
haidohong <dohaimail@gmail.com>
2023-01-17 17:11:13 +0700

    Evaluate objective value

---
+++ generate_graph.cpp
@@ -0,0 +1,28 @@
+#include<vector>
+#include<iostream>
+#include<fstream>
+#define MAX_DIS 18
+#define MIN_DIS 3
+#define MAX_P 100
+using namespace std;
+int main(){
+    ofstream fo("graph.txt");
+    srand(time(NULL));
+    int n = 5;
+    double graph[MAX_P][MAX_P];
+    for (int i = 0; i <= n; ++i){
+        graph[i][i] = 0;
+        for (int j = i + 1; j <= n; ++j){
+            graph[i][j] = rand() % (MAX_DIS - MIN_DIS) + MIN_DIS;
+            graph[j][i] = graph[i][j];
+        }
+    }
+    fo << n << "\n";
+    for (int i = 0; i <= n; ++i){
+        for (int j = 0; j <= n; ++j){
+            fo << graph[i][j] << "\t";
+        }
+        fo << endl;
+    }
+
+}
--- init_route.cpp
+++ init_route.cpp
@@ -1,21 +1,42 @@
 #include<iostream>
 #include<vector>
+#include<fstream>
+#define NUM_OF_TECHNICIANS 2
+#define NUM_OF_POINTS 5
+#define MAX_P 100
+#define SPEED 5
+
 using namespace std;
+
+double graph[MAX_P][MAX_P];
 vector<int> generate_permutation(int n){
     vector<int> permutation(n);
     for (int i = 0; i < n; ++i) permutation[i] = i + 1;
     for (int i = 0; i < n; ++i) swap(permutation[i], permutation[rand() % n]);
     return permutation;
 }
+void read_graph(){
+    ifstream fi("graph.txt");
+    int n;
+    fi >> n;
+    for (int i = 0; i <= n; ++i){
+        for (int j = 0; j <= n; ++j){
+            fi >> graph[i][j];
+        }
+    }
+}
 class Technician{
 public:
+    double total_wait_time;
     Technician();
     void add_point(int point);
     void print_route();
     void end_route();
 private:
     vector<int> route;
+    double evaluate_wait_time();
 };
+
 Technician::Technician() {
     (this -> route).push_back(0);
 }
@@ -24,6 +45,8 @@ void Technician::add_point(int point){
 }
 void Technician::end_route(){
     (this -> route).push_back(0);
+    this -> total_wait_time = this -> evaluate_wait_time();
+
 }
 void Technician::print_route(){
     int cnt_point = 0;
@@ -31,28 +54,58 @@ void Technician::print_route(){
         if (cnt_point++ > 0) cout << " -> ";
         cout << point;
     }
-    cout << endl;
+    cout << "\tTOTAL WAIT TIME: " << this -> total_wait_time <<endl;
 }
-void assign_route(vector<int>& route, vector<Technician>& technicians, int num_of_points){
-    int current_technician = 0;
-    for (int point : route) {
-        if (point > num_of_points) {
-            technicians[current_technician].end_route();
-            ++current_technician;
+double Technician::evaluate_wait_time(){
+    double total_distance = 0;
+    int n = (this -> route).size() - 1;
+    for (int i = 1; i < n ; ++i) total_distance += i * graph[(this -> route)[i]][(this -> route)[i + 1]];
+    return total_distance / SPEED;
+}
+
+class Solution{
+public:
+    double objective_value;
+    Solution();
+    void show_solution();
+private:
+    vector<int> route;
+    vector<Technician> technicians;
+    double evaluate_objective_value();
+    void assign_route();
+};
+Solution::Solution(){
+    this -> route = generate_permutation(NUM_OF_POINTS + NUM_OF_TECHNICIANS - 1);
+    assign_route();
+    this -> objective_value = evaluate_objective_value();
+}
+void Solution::assign_route(){
+    Technician tmp_technician = Technician();
+    for (int point : this -> route) {
+        if (point > NUM_OF_POINTS) {
+            tmp_technician.end_route();
+            (this -> technicians).push_back(tmp_technician);
+            tmp_technician = Technician();
             continue;
         }
-        technicians[current_technician].add_point(point);
+        tmp_technician.add_point(point);
     }
-    technicians[current_technician].end_route();
-
+    tmp_technician.end_route();
+    (this -> technicians).push_back(tmp_technician);
+}
+double Solution::evaluate_objective_value(){
+    double value = 0;
+    for (Technician tech : this -> technicians) value += tech.total_wait_time;
+    return value;
+}
+void Solution::show_solution(){
+    for (Technician tech : this -> technicians) tech.print_route();
+    cout << "OBJECTIVE VALUE: " << this -> objective_value << endl;
 }
 int main(){
     srand(time(NULL));
-    int num_of_technicians = 2, num_of_points = 10;
-    vector<int> init_route = generate_permutation(num_of_points + num_of_technicians - 1);
-    vector<Technician> technicians(num_of_technicians);
-    assign_route(init_route, technicians, num_of_points);
-    for (Technician tech : technicians){
-        tech.print_route();
-    }
+    read_graph();
+    vector<int> init_route = generate_permutation(NUM_OF_POINTS + NUM_OF_TECHNICIANS - 1);
+    Solution sol1 = Solution();
+    sol1.show_solution();
 }
